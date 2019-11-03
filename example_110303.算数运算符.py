cd1d8aa23a5d642d82f2096a7314556da534b233
blackplusy <osimeno@163.com>
2019-11-03 11:39:12 +0800

    Add files via upload

---
+++ example_110303.算数运算符.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+a=100
+b=3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+
---
+++ example_110305.逻辑运算符.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+print(1>2 and 2<3)
+#真 and 假  假
+
+print(1>2 or 2<3)
+#真 or  假  真
+
+print(not(1>3))
+
+#非假  真
